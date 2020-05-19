5d7e007673ebaad5e4fa639f884d32f7a2685970
jotagehes <joaogabriel-12@hotmail.com>
2020-05-19 10:28:39 -0300

    Atualizacao das médias

--- cod.js
+++ cod.js
@@ -1,21 +1,50 @@
 /*Funções criadas em JavaScript*/
 alert("Oi");
 confirm("Vamos acessar?");
-prompt("Qual o seu nome?");
+var nome = prompt("Informe seu nome completo separado por espaços:");
 let ligado = true;
+nomes = nome.split(" ");
+console.log(nomes);
+var i = 0;
+
+
+for (i < nomes.lenght; i++;) {
+    console.log(nomes[i] + "<br>")
+}
 
 function deligarTudo() {
     if (ligado == true) {
         apagarLamp();
-        ligado = false
+        ligado = false;
         alert("Você está sem energia!");
-        document.getElementById("meuclick").textContent="Reativar Energia";
+        document.getElementById("meuclick").textContent = "Reativar Energia";
     } else {
         alert("A energia será restaurada");
         ligado = true;
         ligarLamp();
-        document.getElementById("meuclick").textContent="Desativar Energia";
+        document.getElementById("meuclick").textContent = "Desativar Energia";
     }
+
+}
+function sayhi() {
+    let nome = document.getElementById("in-nome").value;
+    let sobrenome = document.getElementById("in-snome").value;
+    alert("Olá " + nome + " " + sobrenome)
+}
+function calcmedia() {
+    var notastt = document.getElementById("in-notas");
+    var medias = notastt.value.split(",");
+    console.log(medias);
+    let resultado = 0;
+    let divisor = medias.lenght;
+    for (j = 0; j < medias.lenght; j++) {
+
+        resultado = medias[j] + resultado;
+        console.log(resultado);
+        console.log("Socorro");
+    }
+    console.log(resultado);
+    alert("A média das notas é: " + resultado / divisor);
 
 }
 function verificar() {
@@ -23,12 +52,12 @@ function verificar() {
 }
 function ligarLamp() {
     if (ligado == true) {
-        document.getElementById("imlamp").src="https://i.stack.imgur.com/ybxlO.jpg";
+        document.getElementById("imlamp").src = "https://i.stack.imgur.com/ybxlO.jpg";
     }
 
 }
 function apagarLamp() {
-    document.getElementById("imlamp").src="https://i.stack.imgur.com/b983w.jpg";
+    document.getElementById("imlamp").src = "https://i.stack.imgur.com/b983w.jpg";
 
 
 }
